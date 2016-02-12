a63e1d74b1779e36ec25e84dbc99f840ac17982f
cnngimenez <cgimenez.public@openmailbox.org>
2016-02-12 12:17:49 -0300

    Agregada una página inicial.

---
+++ web-src/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Inicio</title>
+	<meta name="author" content="Giménez, Christian">
+	<link rel="stylesheet" href="./css/joint.min.css" />
+	<link rel="stylesheet" href="./css/jquery.mobile-1.4.5.min.css" />
+	<script src="./js/jquery.min.js"></script>
+	<script src="./js/jquery.mobile-1.4.5.min.js"></script>
+    </head>
+
+    <body>
+	<div data-role="page">
+	    <div data-role="header">		
+		<h1>WICOM</h1>
+	    </div><!-- /header -->
+
+	    <div role="main" class="ui-content">
+		<div data-role="controlgroup" data-type="vertical">
+		    <a class="ui-btn ui-icon-gear ui-btn-icon-left" data-ajax="false"
+		       href="./web.php">Prueba de conexión entre Racer y esta web</a>
+		    <a class="ui-btn ui-icon-edit ui-btn-icon-left" data-ajax="false"
+		       href="./interfaz.php">Editar un diagrama</a>
+		</div>
+	    </div> <!-- /main -->
+	    
+	    <div data-role="footer">	
+		<address>
+		    <a href="mailto:christian@Harmonia">Giménez, Christian</a>,
+		    12 feb 2016
+		</address>
+	    </div><!-- /footer -->
+	</div><!-- /page -->
+    </body>
+</html>
