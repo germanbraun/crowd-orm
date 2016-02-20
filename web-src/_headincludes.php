eddebdc615bfaaadd6c5568dd3fe4a4aa79ea406
cnngimenez <cgimenez.public@openmailbox.org>
2016-02-19 22:48:21 -0300

    Fixing the config.php path.

--- web-src/_headincludes.php
+++ web-src/_headincludes.php
@@ -1,5 +1,5 @@
 <?php
-include './config.php'
+include './config/config.php'
 ?>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
