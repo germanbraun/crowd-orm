bbbfe13c8e082de9ae56d9c0d3a12db26446aa4d
Christian Gimenez <cnngimenez@gmail.com>
2016-03-10 18:00:19 -0300

    Completing the CalvanesseTest class.

--- web-src/tests/translator/calvanessetest.php
+++ web-src/tests/translator/calvanessetest.php
@@ -22,11 +22,26 @@
 
 class CalvanesseTest extends PHPUnit_Framework_TestCase
 {
+
     public function testTranslate(){
         //TODO: Complete JSON!
-        $a = json_decode('{"clases": [{"attrs":[], "methods":[], "name": "Hola Mundo"}]}');
+        $a = json_decode('{"clases": [{"attrs":[], "methods":[], "name": "Hi World"}]}');
         //TODO: Complete XML!
-        $xml = "";
+        $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<RequestMessage xmlns=\"http://www.owllink.org/owllink#\"
+		xmlns:owl=\"http://www.w3.org/2002/07/owl#\" 
+		xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+		xsi:schemaLocation=\"http://www.owllink.org/owllink# 
+				    http://www.owllink.org/owllink-20091116.xsd\">
+  <CreateKB kb=\"http://localhost/kb1\" />
+  <Tell kb=\"http://localhost/kb1\">   
+    <owl:SubClassOf>
+      <owl:Class IRI=\"Hi World\" />
+      <owl:Class abbreviatedIRI=\"owl:Thing\" />
+    </owl:SubClassOf>
+  </Tell>
+  <ReleaseKB kb=\"http://localhost/kb1\" />
+</RequestMessage>";
 
         $t = new Translator(new Calvanesse(), new XMLOwlBuilder());
         $res = $t->translate($a);
