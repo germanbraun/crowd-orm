4dfcdb6842bc4eb6848426a5cf926ab92ce86cad
christian <christian@minerva>
2016-03-15 21:43:49 -0300

    Fixing owllinkanalizer.php, better filter the XML separating the important tags from the non-relevants.

--- web-src/answers/owllinkanalizer.php
+++ web-src/answers/owllinkanalizer.php
@@ -44,11 +44,12 @@ class OWLlinkAnalizer extends AnsAnalizer{
 
     /**
        Map between Queries and propper correct answers.
+
+       Used for filtering XML tags to the ones we care.
      */
     const ANSWERS_MAP = [
-        "CreateKB" => "KB",
-        "Tell" => "OK",
-        "IsKBSatisfiable" => "BooleanResponse"
+        "IsKBSatisfiable" => "BooleanResponse",
+        "IsClassSatisfiable" => "BooleanResponse"
     ];
     
     function __construct($query, $answer){
@@ -58,79 +59,163 @@ class OWLlinkAnalizer extends AnsAnalizer{
         $this->answer_reader = XMLReader::xml($answer);
     }
 
-    function analize(){
-        $this->query_reader->next();
-        $this->answer_reader->next();
+    /**
+       Remove comments and other tags, and create an array with 
+       the most important tags and its answer.
+       
+       @return An Asociated Array with most important tags and 
+       its answer. Example:
+
+       @code{.php}
+       [
+         "IsKBSatisfiable" => "true", 
+         "IsClassSatisfiable" => "true"
+       ]       
+       @endcode
+     */
+    function filter_xml(){
+        $cont = true;
+        $col = [
+            "IsKBSatisfiable" => "false",
+            "IsClassSatisfiable" => []
+        ];
+        
+        // Enter the first tag
+        $this->next_tag();
+        $this->next_tag(false);
         $this->query_reader->read();
         $this->answer_reader->read();
-        while ($this->query_reader->next()){
-            
-            // Ignore spaces, comments, or whatever thing except an ELEMENT
-            while ($this->query_reader->nodeType != XMLReader::ELEMENT){
-                if (! $this->query_reader->next()){
-                    return;
-                }
-            }
-            // Next answer_reader element:
-            if (! $this->answer_reader->next()){
-                return;
-            }
-            // Ignore spaces, comments or whatever thing appears except an ELEMENT.
-            while ($this->answer_reader->nodeType != XMLReader::ELEMENT){
-                if (! $this->answer_reader->next()) {
-                    return;
-                }
+
+        while ($this->next_important_tag()){
+            if (! $this->next_important_tag(false)){
+                return $col;
             }
 
-            /*
-            print("\n\n--------------------");
-            print($this->query_reader->name);
-            print(" -> ");
-            print($this->answer_reader->name);
-            */            
+            $a_name = $this->answer_reader->name;
+            $q_name = $this->query_reader->name;
             
-            $q_tagname = $this->query_reader->name;
-            
-            // ...Checking for proper queries...
-            
-            if ($q_tagname == "IsKBSatisfiable"){
-                // <BooleanResponse result="true" />
-                $isit = $this->answer_reader->getAttribute("result");
-                
-                if ($isit == "true"){
-                    $this->answer->set_kb_satis(true);
-                }else{
-                    $this->answer->set_kb_satis(false);
-                }
-            }
-
-            if ($q_tagname == "IsClassSatisfiable"){
-                /* In Query:
-                       <IsClassSatisfiable kb="KB URI">
-                           <owl:Class URI="URI OR CLASS NAME" />
-                       </IsClassSatisfiable>
-
-                   In Answer: <BooleanResponse result="true" />
-                */
-                
-                // Get classname
-                while ($this->query_reader->read() and
-                       $this->query_reader->nodeType != XMLReader::ELEMENT){}
-                    
-                $classname = $this->query_reader->getAttribute("IRI");
-                
-                // Get results
+            if ($a_name == "BooleanResponse") {
                 $isit = $this->answer_reader->getAttribute("result");
 
-                if ($isit == "true"){
-                    $this->answer->add_satis_class($classname);
-                }else{
-                    $this->answer->add_unsatis_class($classname);
+                if ($q_name == "IsClassSatisfiable"){
+                    $classname = $this->find_classname();                    
+                    array_push($col[$q_name], [$isit, $classname]);
+                }else{  // $q_name == "IsKBSatisfiable"              
+                    $col[$q_name] = $isit;
                 }
             }
-            
-            
         }
+        
+        return $col;
     }
+
+    protected function find_classname(){
+        $this->query_reader->read();
+        while ($this->query_reader->name != "owl:Class"){
+            if (! $this->next_tag()){
+                return false;
+            }
+        }
+
+        $classname = $this->query_reader->getAttribute("IRI");
+        
+        return $classname;
+    }
+
+    /**
+       Simply, go to the next initial tag.
+
+       @param in_query A boolean, if true advance the query_reader,
+       if false, advance the answer_reader.
+
+       @return false if the EOF has been reached.
+     */
+    protected function next_tag($in_query = true){
+        $cont = true;        
+        if ($in_query){
+            $xml = $this->query_reader;
+        }else{
+            $xml = $this->answer_reader;
+        }
+        
+        while ($cont){
+            if (!$xml->next()){
+                return false;
+            }
+            $cont = $xml->nodeType != XMLReader::ELEMENT;
+        }
+        return true;
+    }
+
+    /**
+       @param in_query A Boolean indicating if it must advance the 
+       $this->query_reader XMLReader instance (true) or the 
+       $this->answer_reader one (false).
+
+       @return false if the EOF has been reached. True otherwise.
+     */
+    protected function next_important_tag($in_query=true){
+        $cont = true;
+
+        if ($in_query){
+            $xml = $this->query_reader;
+        }else{
+            $xml = $this->answer_reader;
+        }            
+        
+        while ($cont){
+            if (! $xml->next()){
+                // Cannot read more, EOF?
+                return false;
+            }
+
+            // If it is not an ELEMENT, repeat.
+            $cont = $xml->nodeType != XMLReader::ELEMENT;
+
+            // If it has not the tagname we're looking for, repeat.
+            if (!$cont){
+                // It is in fact an element.
+                if($in_query){
+                    $cont = ! array_key_exists($xml->name,
+                                               OWLlinkAnalizer::ANSWERS_MAP);
+                }else{
+                    $cont = ! array_search($xml->name,
+                                           OWLlinkAnalizer::ANSWERS_MAP);
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+
+    
+    function analize(){
+
+        /*
+        print("\n\n ________________________________________\n");
+        print("analize() ---------->\n\n");
+        print_r($this->answer);
+        */
+        
+        $col = $this->filter_xml();
+
+        //print_r($col);
+
+        
+        $val = $col["IsKBSatisfiable"];
+        $this->answer->set_kb_satis($val == "true");                    
+
+        $col_class = $col["IsClassSatisfiable"];
+        foreach ($col_class as $val){
+            if ($val[0] == "true"){
+                $this->answer->add_satis_class($val[1]);
+            }else{
+                $this->answer->add_unsatis_class($val[1]);
+            }        
+        }
+        return $this->answer;
+    }                    
+    
 }
 
