0860dd07592def5ca671cd84ffea4fbd48d667b0
christian <christian@minerva>
2016-03-15 21:44:30 -0300

    :bar_chart: making some more test for testing the filter_xml function.

--- tests/php/answers/owllinkanalizertest.php
+++ tests/php/answers/owllinkanalizertest.php
@@ -31,6 +31,76 @@ use Wicom\Answers\OWLlinkAnalizer;
 class OWLlinkAnalizerTest extends PHPUnit_Framework_TestCase
 {
 
+    public function testFilterXML(){
+        $query_input = <<<'EOT'
+<?xml version="1.0" encoding="UTF-8"?>
+<RequestMessage xmlns="http://www.owllink.org/owllink#"
+		xmlns:owl="http://www.w3.org/2002/07/owl#" 
+		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		xsi:schemaLocation="http://www.owllink.org/owllink# 
+				    http://www.owllink.org/owllink-20091116.xsd">
+  <CreateKB kb="http://localhost/kb1" />
+  <Tell kb="http://localhost/kb1">
+    <owl:SubClassOf>
+      <owl:Class IRI="Person" />
+      <owl:Class abbreviatedIRI="owl:Thing" />
+    </owl:SubClassOf>
+    <owl:SubClassOf>
+      <owl:Class IRI="OtherPerson" />
+      <owl:Class abbreviatedIRI="owl:Thing" />
+    </owl:SubClassOf>
+    <owl:SubClassOf>
+      <owl:Class IRI="Nope this one nope" />
+      <owl:Class abbreviatedIRI="owl:Thing" />
+    </owl:SubClassOf>
+  </Tell>
+  
+  <!-- Queries -->
+  
+  <IsKBSatisfiable kb="http://localhost/kb1" />
+  <IsClassSatisfiable kb="http://localhost/kb1">
+    <owl:Class IRI="Person" />
+  </IsClassSatisfiable>
+  <IsClassSatisfiable kb="http://localhost/kb1">
+    <owl:Class IRI="OtherPerson" />
+  </IsClassSatisfiable>
+  <IsClassSatisfiable kb="http://localhost/kb1">
+    <owl:Class IRI="Nope this one nope" />
+  </IsClassSatisfiable>
+
+
+  
+  <ReleaseKB kb="http://localhost/kb1" />
+</RequestMessage>
+EOT;
+        $answer_output = <<<'EOT'
+<?xml version="1.0" encoding="UTF-8"?>
+<ResponseMessage xmlns="http://www.owllink.org/owllink#"
+                 xmlns:owl="http://www.w3.org/2002/07/owl#">
+  <KB kb="http://localhost/kb1"/>
+  <OK/>
+  <BooleanResponse result="true"/>
+  <BooleanResponse result="true"/>
+  <BooleanResponse result="true"/>
+  <BooleanResponse result="false"/>
+  <OK/>
+</ResponseMessage>
+EOT;
+
+        $expected = [ "IsKBSatisfiable" => "true",
+                      "IsClassSatisfiable" => [
+                          ["true", "Person"],
+                          ["true", "OtherPerson"],
+                          ["false", "Nope this one nope"]
+                      ]
+        ];
+        
+        $oa = new OWLlinkAnalizer($query_input, $answer_output);
+        $actual = $oa->filter_xml();
+        
+        $this->assertEquals($expected, $actual, true);
+    }
+
     public function testAnswerJson(){
         $query_input = <<<'EOT'
 <?xml version="1.0" encoding="UTF-8"?>
@@ -95,6 +165,126 @@ EOT;
            },
            "unsatisfiable": {
               	"classes" : ["Nope this one nope"]
+           },
+           "suggestions" : {
+              	"links" : []
+           },
+           "reasoner" : {
+              	"input" : "",
+              	"output" : ""
+           }
+       }
+EOT;
+
+        $oa = new OWLlinkAnalizer($query_input, $answer_output);
+        $oa->analize();
+        $answer = $oa->get_answer();
+        // Removing input and output XML string, is merely descriptive.
+        $answer->set_reasoner_input("");
+        $answer->set_reasoner_output("");
+        $actual = $answer->to_json();
+
+
+        /*
+        print("\n\n");
+        print($actual);
+        print("\n\n");
+        */
+        
+        $this->assertJsonStringEqualsJsonString($expected, $actual, true);
+    }
+
+    public function testSimpleOWLlink(){
+        $query_input = <<<'EOT'
+<?xml version="1.0" encoding="UTF-8"?>
+<RequestMessage xmlns="http://www.owllink.org/owllink#"
+		xmlns:owl="http://www.w3.org/2002/07/owl#" 
+		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		xsi:schemaLocation="http://www.owllink.org/owllink# 
+				    http://www.owllink.org/owllink-20091116.xsd">
+  <CreateKB kb="http://localhost/kb1" />
+  <Tell kb="http://localhost/kb1">
+    <owl:SubClassOf>
+      <owl:Class IRI="Person" />
+      <owl:Class abbreviatedIRI="owl:Thing" />
+    </owl:SubClassOf>
+  </Tell>
+  
+  <!-- Queries -->
+  
+  <IsKBSatisfiable kb="http://localhost/kb1" />
+  <IsClassSatisfiable kb="http://localhost/kb1">
+    <owl:Class IRI="Person" />
+  </IsClassSatisfiable>
+  
+  <ReleaseKB kb="http://localhost/kb1" />
+</RequestMessage>
+EOT;
+        $answer_output = <<<'EOT'
+<?xml version="1.0" encoding="UTF-8"?>
+<ResponseMessage xmlns="http://www.owllink.org/owllink#"
+                 xmlns:owl="http://www.w3.org/2002/07/owl#">
+  <KB kb="http://localhost/kb1"/>
+  <OK/>
+  <BooleanResponse result="true"/>
+  <BooleanResponse result="true"/>
+  <OK/>
+</ResponseMessage>
+EOT;
+
+        $expected = <<<'EOT'
+       {
+           "satisfiable": {
+               "kb" : true,
+               "classes" : ["Person"]
+           },
+           "unsatisfiable": {
+              	"classes" : []
+           },
+           "suggestions" : {
+              	"links" : []
+           },
+           "reasoner" : {
+              	"input" : "",
+              	"output" : ""
+           }
+       }
+EOT;
+
+        $oa = new OWLlinkAnalizer($query_input, $answer_output);
+        $oa->analize();
+        $answer = $oa->get_answer();
+        // Removing input and output XML string, is merely descriptive.
+        $answer->set_reasoner_input("");
+        $answer->set_reasoner_output("");
+        $actual = $answer->to_json();
+
+
+        /*
+        print("\n\n");
+        print($actual);
+        print("\n\n");
+        */
+        
+        $this->assertJsonStringEqualsJsonString($expected, $actual, true);
+    }
+    public function testRealCase(){
+        $query_input = <<<'EOT'
+<?xml version="1.0" encoding="UTF-8"?>
+<RequestMessage xmlns="http://www.owllink.org/owllink#" xmlns:owl="http://www.w3.org/2002/07/owl#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.owllink.org/owllink# http://www.owllink.org/owllink-20091116.xsd"><CreateKB kb="http://localhost/kb1"/><Tell kb="http://localhost/kb1"><owl:SubClassOf><owl:Class IRI="Hi World"/><owl:Class abbreviatedIRI="owl:Thing"/></owl:SubClassOf></Tell><IsKBSatisfiable kb="http://localhost/kb1"/><IsClassSatisfiable kb="http://localhost/kb1"><owl:Class IRI="Hi World"/></IsClassSatisfiable></RequestMessage>
+EOT;
+        $answer_output = <<<'EOT'
+<?xml version="1.0" encoding="UTF-8"?><ResponseMessage xmlns="http://www.owllink.org/owllink#"                 xmlns:owl="http://www.w3.org/2002/07/owl#">  <KB kb="http://localhost/kb1"/>  <OK/>  <BooleanResponse result="true"/>  <BooleanResponse result="true"/></ResponseMessage>
+EOT;
+
+        $expected = <<<'EOT'
+       {
+           "satisfiable": {
+               "kb" : true,
+               "classes" : ["Hi World"]
+           },
+           "unsatisfiable": {
+              	"classes" : []
            },
            "suggestions" : {
               	"links" : []
