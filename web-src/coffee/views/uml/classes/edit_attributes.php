5c5aadb15a454280490f6392e17d84ce64bce3d7
christian <christian@fi>
2017-10-27 16:16:45 -0300

    Implementing Attributes for UML classes

---
+++ web-src/coffee/views/uml/classes/edit_attributes.php
@@ -0,0 +1,33 @@
+<?php 
+/* 
+
+   Copyright 2017 Giménez, Christian
+   
+   Author: Giménez, Christian   
+
+   edit_attributes.php
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+?>
+<form>
+    <div class="" id="umlattr_list">
+	
+    </div>
+    <input data-mini="true" placeholder="Attribute description"  type="text" id="umlattr_input" />
+    <a class="ui-btn ui-corner-all ui-icon-check ui-btn-icon-notext" type="button" id="umladd_attr_button">Add</a>
+    <a class="ui-btn ui-corner-all ui-icon-back ui-btn-icon-notext"  type="button" id="umlback_attr_button">Back</a>    
+</form>
